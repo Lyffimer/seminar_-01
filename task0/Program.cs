57b6b8901581441e4e0536398d69df91f687a7b4
Igor_Deulin <lyffimer@bk.ru>
2022-12-02 15:08:23 +0300

    add task and homework

---
+++ task0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+//string answer = Console.ReadLine();
+//int number = Convert.ToInt32(answer);
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);
---
+++ task5/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i = -N; i <= N; i++)
+{
+    Console.Write(i + " ");
+}
